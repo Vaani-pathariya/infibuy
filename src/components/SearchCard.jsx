f6d72df46a8f9765002c0aa9cddbf0d6b019ddd8
Vaani <vaanipathariya@gmail.com>
2023-11-02 23:26:36 +0530

    Setting up basic skeleton

---
+++ src/components/SearchCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const SearchCard=()=>{
+    return (
+        <div>Hello Card</div>
+    )
+}
+export default SearchCard;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import data from "../data/products.json"
+import SearchCard from "../components/SearchCard";
 const Search=()=>{
+
+    console.log(data[0]['img']);
     return (
         <div>
             <Navbar/>
             Hello I am search page
+            {/* <img src={data[0]['img']}></img> */}
+            <SearchCard/>
         </div>
     )
 }
